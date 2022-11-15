a6b88d300be27fdb3a97150e216f41f1d609000d
jumsanchezo <jumsanchezo@correo.udistrital.edu.co>
2022-11-14 21:35:30 -0500

    multiplayer-connection

---
+++ FirstProject2D/Assets/LogicaRed.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LogicaRed : MonoBehaviourPunCallbacks
+{
+
+    public static LogicaRed instancia;
+
+
+    void Awake(){
+        instancia = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster(){
+        Debug.Log("Felicidades!! estás conectado! ");
+    }
+}
---
+++ FirstProject2D/Assets/Scripts/GestorPhoton.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GestorPhoton : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster(){
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby(){
+        PhotonNetwork.JoinOrCreateRoom("Cuarto", new RoomOptions { MaxPlayers = 2}, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom(){
+        PhotonNetwork.Instantiate("Ryan", new Vector2(Random.Range(-7,-3),3), Quaternion.identity);
+    }
+}
