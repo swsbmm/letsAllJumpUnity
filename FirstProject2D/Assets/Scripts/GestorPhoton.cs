2e41c80f8dd7010fd5a52ff10fb567972a6fd63a
jumsanchezo <jumsanchezo@correo.udistrital.edu.co>
2022-12-05 00:30:22 -0500

    menu de sala e ingreso

--- FirstProject2D/Assets/Scripts/GestorPhoton.cs
+++ FirstProject2D/Assets/Scripts/GestorPhoton.cs
@@ -2,14 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using Photon.Realtime;
+using Photon.Realtime; 
+using UnityEngine.UI;
+
 
 public class GestorPhoton : MonoBehaviourPunCallbacks
 {
+    public GameObject[] EntryButtons;
+
+    private TypedLobby customLobby = new TypedLobby("customLobby", LobbyType.Default);
+
+    private Dictionary<string, RoomInfo> cachedRoomList = new Dictionary<string, RoomInfo>();
+
+    static string PlayerNamePrefKey = "PlayerName";
+
+    public InputField NameInputField;
+
+    string RoomName = "place holder";
+
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        if(!PhotonNetwork.IsConnected){
+            PhotonNetwork.AutomaticallySyncScene = true;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        string DefaultName = "";
+        //InputField NameInputField = GetComponent<InputField>();
+        if(NameInputField != null){
+            if(PlayerPrefs.HasKey(PlayerNamePrefKey)){
+                DefaultName = PlayerPrefs.GetString(PlayerNamePrefKey);
+                NameInputField.text = DefaultName;
+            }
+        }
+        PhotonNetwork.NickName = DefaultName;
     }
 
     // Update is called once per frame
@@ -18,15 +45,102 @@ public class GestorPhoton : MonoBehaviourPunCallbacks
         
     }
 
+    public override void OnPlayerEnteredRoom( Player newPlayer){
+        print(newPlayer.NickName + "Has joined");
+
+        if(PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount >=2){
+            PhotonNetwork.LoadLevel(2);
+        }
+    }
+
     public override void OnConnectedToMaster(){
+        print("connected to master");
         PhotonNetwork.JoinLobby();
     }
 
+    //public override void OnJoinedLobby(){
+    //    PhotonNetwork.JoinOrCreateRoom("Cuarto", new RoomOptions { MaxPlayers = 2}, TypedLobby.Default);
+    //}
+
     public override void OnJoinedLobby(){
-        PhotonNetwork.JoinOrCreateRoom("Cuarto", new RoomOptions { MaxPlayers = 2}, TypedLobby.Default);
+        //PhotonNetwork.JoinLobby(customLobby);
+        print("connected to Lobby");
+    }
+
+    public void CreateRoom(){
+        PhotonNetwork.CreateRoom(RoomName, new RoomOptions() {MaxPlayers = 2}, null );
     }
 
     public override void OnJoinedRoom(){
-        PhotonNetwork.Instantiate("Ryan", new Vector2(-7,-2.6f), Quaternion.identity);
+        print("Joined room");
+        //PhotonNetwork.Instantiate("Ryan", new Vector2(-7,-2.6f), Quaternion.identity);
+    }
+
+    public void GetRoomList(){
+        print("tamaño"+ cachedRoomList.Count);
+        RoomInfo[] RoomList = new RoomInfo[cachedRoomList.Count];
+        int Position = 0;
+        Dictionary<string, RoomInfo>.ValueCollection values = cachedRoomList.Values;  
+        foreach (RoomInfo val in values)  {  
+            RoomList[Position]=val;  
+            Position = Position + 1;   
+        }
+
+        if(RoomList.Length > 0){
+            
+            for(int Index = 0; Index < RoomList.Length; ++Index){
+                RoomInfo Room = RoomList[Index];
+                GameObject Entry = EntryButtons[Index];
+                Text EntryText = Entry.GetComponentInChildren<Text>();
+                EntryText.text = Room.Name;
+            }
+
+        }
+
+    }
+
+    private void UpdateCachedRoomList(List<RoomInfo> roomList)
+    {
+        for(int i=0; i<roomList.Count; i++)
+        {
+            RoomInfo info = roomList[i];
+            if (info.RemovedFromList)
+            {
+                cachedRoomList.Remove(info.Name);
+            }
+            else
+            {
+                cachedRoomList[info.Name] = info;
+            }
+        }
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        UpdateCachedRoomList(roomList);
+    }
+
+    public override void OnLeftLobby()
+    {
+        cachedRoomList.Clear();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        cachedRoomList.Clear();
+    }
+
+    public void JoinRoom(Text EntryText){
+        PhotonNetwork.JoinRoom(EntryText.text);
+    }
+
+    public void SetPlayerName(string value){
+        PhotonNetwork.NickName = value + " ";
+        PlayerPrefs.SetString(PlayerNamePrefKey, value);
+
+    }
+
+    public void SetRoomName(string value){
+        RoomName = value;
     }
 }
